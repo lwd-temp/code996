7c07f0a9116a70e655cd2661455e819517d95c41
digua <hellodigua@gmail.com>
2022-05-10 00:35:32 +0800

    feat: 打包体积分析

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import externalGlobals from 'rollup-plugin-external-globals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), visualizer()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
